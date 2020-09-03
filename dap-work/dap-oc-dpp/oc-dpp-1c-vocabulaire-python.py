800bf21a02dc20c6b2bb42fa84a7666b41bee272
Eri Schon <erischon@gmail.com>
2020-09-03 12:15:15 +0200

    mes notes sur le cours 1c decouvrez le vocabulaire python

--- dap-work/dap-oc-dpp/oc-dpp-1c-vocabulaire-python.py
+++ dap-work/dap-oc-dpp/oc-dpp-1c-vocabulaire-python.py
@@ -4,37 +4,37 @@
     # integer
 
 nbe_int = 1
-print("Objet, nombre integer :", nbe_int)
+print("Objet, nombre integer :", nbe_int, "\n")
 
     # float
 
 nbe_float = 1.5
-print("Objet, nombre float :", nbe_float)
+print("Objet, nombre float :", nbe_float, "\n")
 
 # 2 - strings
 
 string = "Moi je suis un string"
-print("Object, string: ", string)
+print("Object, string: ", string, "\n")
 
 # 3 - booleans
 
 boolean = 5 > 25
-print("Objet, boolean :", boolean)
+print("Objet, boolean :", boolean, "\n")
 
 # 4 - list
 
 liste = ["item1", "item2"]
 print("Objet, liste :", liste)
-print("l'item 2 d'une liste :", liste[1])
+print("l'item 2 d'une liste :", liste[1], "\n")
 
 # 5 - tuples
 
 tuple = ("element 1", "element 2")
-print("Objet, tuple :", tuple)
+print("Objet, tuple :", tuple, "\n")
 
 # 6 - dictionary
 
 dictionary = {"cle 1": "value 1", "cle 2": "value 2"}
 print("Objet, dictionary :", dictionary)
 print("la value de la cle 1 :", dictionary['cle 1'])
-print("la cle de la value 1 :", dictionary['value 1'])
+print(type(dictionary), "\n")
