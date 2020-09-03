a0054478d9232335863699d41f29a1ef9ef0167d
Eri Schon <erischon@gmail.com>
2020-09-03 12:02:47 +0200

    mes notes sur le cours 1.3 decouvrez le vocabulaire python

---
+++ dap-work/dap-oc-dpp/oc-dpp-13-variables.py
@@ -0,0 +1,40 @@
+# les types d'objets
+
+# 1 - nombres
+    # integer
+
+nbe_int = 1
+print("Objet, nombre integer :", nbe_int)
+
+    # float
+
+nbe_float = 1.5
+print("Objet, nombre float :", nbe_float)
+
+# 2 - strings
+
+string = "Moi je suis un string"
+print("Object, string: ", string)
+
+# 3 - booleans
+
+boolean = 5 > 25
+print("Objet, boolean :", boolean)
+
+# 4 - list
+
+liste = ["item1", "item2"]
+print("Objet, liste :", liste)
+print("l'item 2 d'une liste :", liste[1])
+
+# 5 - tuples
+
+tuple = ("element 1", "element 2")
+print("Objet, tuple :", tuple)
+
+# 6 - dictionary
+
+dictionary = {"cle 1": "value 1", "cle 2": "value 2"}
+print("Objet, dictionary :", dictionary)
+print("la value de la cle 1 :", dictionary['cle 1'])
+print("la cle de la value 1 :", dictionary['value 1'])
