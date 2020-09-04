23a1ae9221bf0f64c8207db8b62f90aaded8f2b0
Eri Schon <erischon@gmail.com>
2020-09-04 10:56:01 +0200

    Learn Python, Python 101 - 9

---
+++ lpp101-work/index_9.py
@@ -0,0 +1,21 @@
+# Strings-basics / Slicing
+
+msg = 'welcome to Python 101: Strings'
+print(msg + msg)
+print(msg * 2)
+print(msg,msg)
+print(msg.upper())
+print(msg.lower())
+print(msg.capitalize())
+print(msg.title())
+
+print(len(msg))
+print(msg.count('Python'))
+
+# slicing
+
+print(msg[0])
+print(msg[-1])
+print(msg[2:])
+print(msg[2:15])
+print(msg[:7])
