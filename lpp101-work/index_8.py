a8b8062e7f7a3d3e6994982c149bb70d86e37b65
Eri Schon <erischon@gmail.com>
2020-09-04 10:34:23 +0200

    Learn Python, Python 101 - 8

---
+++ lpp101-work/index_8.py
@@ -0,0 +1,11 @@
+# Basic Arithmetic
+
+a = 6
+b = 2
+print('Addition : ', a + b)
+print('Subtraction : ', a - b)
+print('Multiplication : ', a * b)
+print('Division (float) : ', a / b)
+print('Division (floor) : ', a // b)
+print('Modulus : ', a % b)
+print('Exponent : ', a ** b)
