5a5581101ac08fa0dfaf4790b32aaf28d7daadfa
Eri Schon <erischon@gmail.com>
2020-09-04 11:47:01 +0200

    Learn Python, Python 101 - 12

---
+++ lpp101-work/index_12.py
@@ -0,0 +1,12 @@
+# User Input
+
+# name = input('What is your name? ')
+# age = input(f'{name}, what is your age? ')
+# print(name)
+# # print('Hello '+ name + '!')
+# print(f'Hello {name}! You are {age} years old.')
+
+num1 = input('Enter a digit: ')
+num2 = input('Enter a second number:')
+answer = float(num1) + float(num2)
+print(answer)
