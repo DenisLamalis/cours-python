37f9d8694c17cb5251a871b47915693f2af4df5e
Eri Schon <erischon@gmail.com>
2020-09-07 12:08:26 +0200

    Learn Python, Python 101 - 22

---
+++ lpp101-work/index_22.py
@@ -0,0 +1,33 @@
+# Comments
+
+#Hiding in the comments
+# more comments
+'''
+Nobody expects the 
+spanish inquisition
+'''
+
+#fruits holds the fruits input by user as strings
+fruits = [‘apple’,’orange’,’banana’,’avocado’]
+
+#Entry form for Ministry applications
+#to-do: fix it! it doesn’t work  
+name = "Default"
+name = input(Enter your silly name: )
+print("Thank you " + name + "!")
+print("for applying to")
+print("the Minstry of Silly Walks")
+
+name = "Default"
+#name = input(Enter your silly name: )
+#print("Thank you " + name + "!")
+print("for applying to")
+print("the Minstry of Silly Walks")
+
+#Entry form for Ministry applications
+#to-do: fix it! it doesn’t work  
+name = "Default"
+name = input("Enter your silly name: ")
+print("Thank you " + name + "!")
+print("for applying to")
+print("the Minstry of Silly Walks")
