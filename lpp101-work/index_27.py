f85b7c19cc39315b2466c4040f534ff96175a585
Eri Schon <erischon@gmail.com>
2020-09-08 10:20:12 +0200

    Learn python, python 101 - 27

---
+++ lpp101-work/index_27.py
@@ -0,0 +1,50 @@
+# Comparisons and Booleans
+
+# a = 7
+# b = 3
+
+# print(a == b)
+# print(a != b)
+# print(a > b)
+# print(a < b)
+# print(a <= b)
+# print(a >= b)
+
+# print('o' in 'john')
+# print('o' not in 'john')
+
+
+# a = [3, 7, 42]
+# # b = a
+# b = [3, 7, 42]
+
+# print(a == b)
+# print(a is b)
+# print(id(a), id(b))
+
+
+# a=7
+# b=3
+# print('a == b is', a == b)
+# print('a != b is', a != b)
+# print('a > b is', a > b)
+# print('a < b is', a < b)
+# print('a >= b is', a >= b)
+# print('a <= b is', a <= b)
+# print('o in John is ','o' in 'John') #membership
+# print('o in John is ','o' not in 'John') #non membership
+# print('John is John ','John' is 'John') #identity
+# print('John is not John is ','John' is not 'John') # negative identity
+
+# print(int(True))
+# print(int(False))
+
+# print(bool(''))
+# print(bool(' '))
+# print(bool(0))
+# print(bool(1))
+# print(bool([]))
+# print(bool([1, 2]))
+
+print(42 + True)
+print(42 + False)
