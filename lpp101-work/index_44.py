a9ced7856e12aef33e6e7c11d333eb6a945b07f7
Eri Schon <erischon@gmail.com>
2020-09-10 14:55:35 +0200

    Learn python, python 101 - 44

---
+++ lpp101-work/index_44.py
@@ -0,0 +1,18 @@
+# Modules
+
+# datetime random string os math browser
+# https://docs.python.org/3/py-modindex.html
+
+# import platform, string, os
+# import platform as pl
+
+# print(dir(platform))
+# print(platform.python_version())
+
+# print(dir(pl))
+
+# from platform import python_version, system
+# print(python_version())
+
+from platform import python_version as pv
+print(pv())
