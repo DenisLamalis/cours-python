a320bf95a38611988bc8a2d5d41e7714c0cdfa4f
Eri Schon <erischon@gmail.com>
2020-09-14 11:22:55 +0200

    in progress

--- environments/env-1/in-progress.py
+++ environments/env-1/in-progress.py
@@ -4,12 +4,12 @@
 current_map = []
 current_map2 = []
 
-map_file = open('map-1.txt','r')
+# map_file = open('map-1.txt','r')
 
-for line in map_file:
-    current_map.append(line.strip().split())
+# for line in map_file:
+#     current_map.append(line.strip().split())
 
-map_file.close()
+# map_file.close()
 
 map_file = open('map-1.txt','r')
 
@@ -19,12 +19,14 @@ for line in map_file:
 map_file.close()
 
 
-map_size = 15
+map_max_x = 15
+map_max_y = 15
 
-print('Map 1 : ', current_map)
+# print('Map 1 : ', current_map)
 print('Map 2 : ',current_map2)
 
-for i in range(map_size):
-    for j in range(map_size): 
+for x in range(map_size):
+    for y in range(map_size): 
         # print('ligne', i+1, current_map[i][0][j])
+        print(f'case x={x},y={y}', current_map2[x][y])
 
