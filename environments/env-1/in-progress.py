b2ae9af307cfe55fae5cefddb031a6606cde4a37
Eri Schon <erischon@gmail.com>
2020-09-14 11:43:55 +0200

    in progress

--- environments/env-1/in-progress.py
+++ environments/env-1/in-progress.py
@@ -2,34 +2,33 @@
 # create the map
 
 current_map = []
-current_map2 = []
-
-# map_file = open('map-1.txt','r')
-
-# for line in map_file:
-#     current_map.append(line.strip().split())
-
-# map_file.close()
 
 map_file = open('map-1.txt','r')
 
 for line in map_file:
-    current_map2.append(line.strip())
+    current_map.append(line.strip())
 
 map_file.close()
-
 
 map_max_x = 15
 map_max_y = 15
 
-# print('Map 1 : ', current_map)
-print('Map 2 : ',current_map2)
+# what I have from the File
+print('Map : ',current_map)
 
+# print the maze
 for x in range(map_max_x):
-    print(current_map2[x])
+    print(current_map[x])
 
+# print each coord of the maze
 for x in range(map_max_x):
     for y in range(map_max_y): 
-        # print('ligne', i+1, current_map[i][0][j])
-        print(f'case x={x},y={y}', current_map2[x][y])
+        print(f'case x={x+1},y={y+1}', current_map[x][y])
+
+# search a coordinate x,y
+
+coord_x = 14
+coord_y = 8
+
+print(f'what is the cell {coord_x},{coord_y} :', current_map[coord_x-1][coord_y-1])
 
