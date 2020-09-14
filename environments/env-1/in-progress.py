719b9c8a4622ba4d5b224b62059473cd8690bcbf
Eri Schon <erischon@gmail.com>
2020-09-14 11:09:17 +0200

    in progress

--- environments/env-1/in-progress.py
+++ environments/env-1/in-progress.py
@@ -2,12 +2,29 @@
 # create the map
 
 current_map = []
+current_map2 = []
 
-map_file = open('map.txt','r')
+map_file = open('map-1.txt','r')
 
 for line in map_file:
-    current_map.append(line.strip().split(','))
+    current_map.append(line.strip().split())
 
 map_file.close()
 
-print(current_map)
+map_file = open('map-1.txt','r')
+
+for line in map_file:
+    current_map2.append(line.strip())
+
+map_file.close()
+
+
+map_size = 15
+
+print('Map 1 : ', current_map)
+print('Map 2 : ',current_map2)
+
+for i in range(map_size):
+    for j in range(map_size): 
+        # print('ligne', i+1, current_map[i][0][j])
+
