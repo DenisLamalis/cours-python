fcab33466f9c2f11bd1bb3984b1495aab71110db
Eri Schon <erischon@gmail.com>
2020-09-17 09:35:16 +0200

    pcc12 in progress

--- pcc-work/12-a-ship-that-fires-bullets/alien_invasion.py
+++ pcc-work/12-a-ship-that-fires-bullets/alien_invasion.py
@@ -22,18 +22,25 @@ class AlienInvasion:
         """ Start the main loop for the game. """
 
         while True:
-            # watch for keyborad and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            # Redraw the screen during each pass through the loop
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            # Make the most recently drawn screen visible
-            pygame.display.flip()
+            self._check_events()
+            self._update_screen()
     
+    def _check_events(self):
+        """ respond to keypresses and mouse events """
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        """ update images on the screen, and flip to the new screen """
+
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # Make the most recently drawn screen visible
+        pygame.display.flip()
+
 if __name__ == '__main__':
     # Make a game instance, and run the game
     ai = AlienInvasion()
