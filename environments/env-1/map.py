fc80ada340c22926d43f935b71eae5498918631e
Eri Schon <erischon@gmail.com>
2020-09-17 14:47:24 +0200

    p3 in progress

--- environments/env-1/map.py
+++ environments/env-1/map.py
@@ -1,18 +1,24 @@
 class Map:
 
     def __init__(self, filename):
-        
-        current_map = []
 
-        map_file = open(filename,'r')
-        for line in map_file:
-            current_map.append(line.strip())
-        map_file.close()
+        self.filename = filename
 
     def find_cell_attribute(x,y):
         return current_map[x-1][y-1]
 
+    def open_file(self):
+        current_map = []
+        
+        map_file = open(self.filename,'r')
+        for line in map_file:
+            current_map.append(line.strip())
+        map_file.close()
+
+        print('Map : ',current_map)
+
+
 
 ma_map = Map('map-1.txt')
 
-print(ma_map)
+print(ma_map.open_file())
