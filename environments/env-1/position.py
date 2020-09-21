a7a53adcc9e38ade88a82b18f644632f6a090bd3
Eri Schon <erischon@gmail.com>
2020-09-21 10:35:08 +0200

    p3 module position

---
+++ environments/env-1/position.py
@@ -0,0 +1,21 @@
+
+class Position:
+    """ Calculate the position """
+    
+    def __init__(self, x, y):
+        self.position = (x, y)
+
+    def __repr__(self):
+        return str(self.position)
+
+    def up(self):
+        return Position(x-1, y)
+
+    def down(self):
+        return Postion(x+1, y)
+
+    def right(self):
+        return Position(x, y+1)
+
+    def left(self):
+        return Position(x, y-1)
