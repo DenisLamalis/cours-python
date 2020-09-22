3caaae7c10a5bd899c2a5b9611b50fb4f55e9b5b
Eri Schon <erischon@gmail.com>
2020-09-22 11:29:11 +0200

    oc-p3 in progress

--- environments/oc-p3/maze.py
+++ environments/oc-p3/maze.py
@@ -40,6 +40,21 @@ class Maze:
         for item in constants.ITEMS:
             self.items.add(item)
 
+    def is_special_cell(self, position):
+        """ Determine if the cell is special """
+        # est-ce que la case est dans start, goal ou items
+        print(f'position envoyee {position}')
+        print(self.start)
+        if {(0,3)} == self.start:
+            print('it is the start')
+        # elif position in self.goal:
+        #     print('it is the goal')
+        # elif position in self.items:
+        #     print('there are a item')
+        else:
+            print('nothing special')
+
+
 #######################
 # Tests & validations #
 #######################
@@ -47,27 +62,30 @@ class Maze:
 def main():
     maze = Maze(constants.FILENAME)
 
-    # test de l'état de cellules
-    p1 = Position(-1,0)
-    print(maze.is_valid_cell(p1))
+    # print('\n')    
+    # # test de l'état de cellules
+    # p1 = Position(-1,0)
+    # p2 = Position(0, 0).right()
+    # p3 = Position(5, 5).right()
 
-    p2 = Position(0, 0).right()
-    print(maze.is_valid_cell(p2))
+    # print(f'Is the cell in position {p1} valide ? {maze.is_valid_cell(p1)}')    
+    # print(f'Is the cell in position {p2} valide ? {maze.is_valid_cell(p2)}')
+    # print(f'Is the cell in position {p3} valide ? {maze.is_valid_cell(p3)}')
 
-    p3 = Position(5, 5).right()
-    print(maze.is_valid_cell(p3))
-
-    # je verifie que j'ai toutes les cases ainsi que leurs coordonnées  
-    print(f'Total des cases : {len(maze.paths) + len(maze.walls)}')
+    # print('\n')
+    # # I validate that there are all the celles and all the coordinates  
+    # print(f'Total of cells : {len(maze.paths) + len(maze.walls)}\n')
     
-    print(f'la liste des positions des {len(maze.paths)} chemins  : {maze.paths}')
-    print(f'la liste des positions des {len(maze.walls)} murs : {maze.walls}')
+    # print(f'Positions of the {len(maze.paths)} paths  : {maze.paths}\n')
+    # print(f'Positions of the {len(maze.walls)} walls : {maze.walls}\n')
 
-    print(f'The start is in this positions : {maze.start}')
-    print(f'The goal is in this positions : {maze.goal}') 
-    print(f'The items are in this positions : {maze.items}')
+    # print(f'The start is in this positions : {maze.start}\n')
+    # print(f'The goal is in this positions : {maze.goal}\n') 
+    # print(f'The items are in this positions : {maze.items}\n')
 
-
+    print('\n')
+    # testing if the cell is special or not
+    maze.is_special_cell({(0,3)})
 
 if __name__ == "__main__":
     main()
