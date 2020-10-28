0b76a14135d1a7748b54368f04a386d690a4cbbb
Eri Schon <erischon@gmail.com>
2020-10-28 11:38:21 +0100

    cw remove consecutive

---
+++ codewars/7-remove-consecutive-duplicate-words.py
@@ -0,0 +1,8 @@
+# Remove consecutive duplicate words
+
+def remove_consecutive_duplicates(s):
+    pass
+
+
+print(remove_consecutive_duplicates('alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'))
+
