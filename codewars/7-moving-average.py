4099e7ab231b946eae83e2f2795222c414e7dac1
Eri Schon <erischon@gmail.com>
2020-10-28 11:25:53 +0100

    cw moving average

--- codewars/7-moving-average.py
+++ codewars/7-moving-average.py
@@ -1,10 +1,38 @@
 # Moving Average
 
 def moving_average(values,n):
-    window = n
-    values = values
+   
+    ws = n
+    value = values
+    sum = 0
+    length = len(value)
+    loop = 0
+    result = []
+    rank = 0
+    
+    while length >= ws:
+        length = length - 1
+        loop = loop + 1
 
-    return #list(floats)
+    if ws == 0 or length < ws:
+        return None
+
+    for x in range(loop):
+
+        for y in range(ws):
+            z = y + rank
+            sum = sum + value[z]
+            
+        rank = rank + 1
+        sum = sum / ws
+        result.append(sum)
+        sum = 0
+
+    return result
 
 
-print(moving_average([40, 30, 50, 46, 39, 44], 3))
+
+print(moving_average([40, 30, 50, 46, 39, 44], 7))
+
+    # if 0 < n <= len(a): return [sum(a[i:i+n]) / n for i in range(len(a) - n + 1)]
+    # return n and [sum(arr[i:i + n]) / n for i in range(len(arr) - n + 1)] or None
