297e7f886d46565c2938b69cac02e6c89c2378c8
Eri Schon <erischon@gmail.com>
2020-10-29 14:52:04 +0100

    cw remove consecutive

--- codewars/7-remove-consecutive-duplicate-words.py
+++ codewars/7-remove-consecutive-duplicate-words.py
@@ -2,20 +2,15 @@
 
 def remove_consecutive_duplicates(words):
     word_list = words.split(" ")
+    index = 0
 
     for y in word_list:
-        index = word_list.index(y)
-        print("y: ",y,"y+1: ",word_list[index+1])
+        index = word_list.index(y, index)
         if y == word_list[index+1]:
             del word_list[index+1]
 
-    for z in word_list:
-        index = word_list.index(z)
-        print("z: ",z,"z+1: ",word_list[index+1])
-        if z == word_list[index+1]:
-            del word_list[index+1]
-
-    print(word_list)
+    word_list = ' '.join(word_list)
+    return word_list
 
 
 print(remove_consecutive_duplicates('alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'))
