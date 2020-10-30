b90e4c564589690c2fa75c7620fd436e870e0f04
Eri Schon <erischon@gmail.com>
2020-10-30 14:40:45 +0100

    cw bonuses

---
+++ codewars/6-bonuses.py
@@ -0,0 +1,16 @@
+# Bonuses
+
+def bonus(arr, s):
+
+    employees = len(arr)
+    total = sum(arr)
+    print(total)
+
+    employee_1 = 
+
+    return arr, s
+
+
+print(bonus([22, 3, 15], 18228))
+
+# [22, 3, 15], 18228, [1860, 13640, 2728]
