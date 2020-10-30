6f0971fb55b27c573ebf2203513599b82a562c01
Eri Schon <erischon@gmail.com>
2020-10-30 14:31:33 +0100

    cw stone on the table

--- codewars/7-stone-on-the-table.py
+++ codewars/7-stone-on-the-table.py
@@ -3,9 +3,16 @@
 
 def solution(stones):
 
-    stones = stones.split()
-    print(stones)
+    length = len(stones)-1
+    number = 0
+
+    for x in range(length):
+        if stones[x] == stones[x+1]:
+            number = number + 1
     return number
 
 
-print(solution('RGBRGBRGGB'))
+print(solution('RGGRGBBRGRR'))
+
+# def solution(s):
+#     return sum(1 for i in range(len(s)) if i and s[i-1]==s[i])
