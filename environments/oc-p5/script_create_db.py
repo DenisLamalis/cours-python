fb3d01262aba0112d5850d9acad685291aa31487
Eri Schon <erischon@gmail.com>
2020-11-12 09:21:14 +0100

    Update script_create_db.py

--- environments/oc-p5/script_create_db.py
+++ environments/oc-p5/script_create_db.py
@@ -1,73 +1,10 @@
 from data import *
 import mysql.connector
 from mysql.connector import errorcode
+from db import Database
 
+db_settings = Database()
 
-# Settings
-
-DB_NAME = 'PureBeurre'
-
-TABLES = {}
-TABLES['categories'] = (
-    "CREATE TABLE IF NOT EXISTS `categories` ("
-    "  `cat_id` int(11) NOT NULL AUTO_INCREMENT,"
-    "  `cat_nom` varchar(100) NOT NULL,"
-    "  PRIMARY KEY (`cat_id`)"
-    ") ENGINE=InnoDB")
-
-TABLES['nutriscore'] = (
-    "CREATE TABLE IF NOT EXISTS `nutriscore` ("
-    "  `nut_id` int(11) NOT NULL AUTO_INCREMENT,"
-    "  `nut_type` char(1) NOT NULL,"
-    "  PRIMARY KEY (`nut_id`)"
-    ") ENGINE=InnoDB")
-
-TABLES['marques'] = (
-    "CREATE TABLE IF NOT EXISTS `marques` ("
-    "  `mar_id` int(11) NOT NULL AUTO_INCREMENT,"
-    "  `mar_nom` varchar(150) NOT NULL,"
-    "  `prodfab_id` int(11) NOT NULL,"
-    "  PRIMARY KEY (`mar_id`)"
-    ") ENGINE=InnoDB")
-
-TABLES['produits'] = (
-    "CREATE TABLE IF NOT EXISTS `produits` ("
-    "  `prod_id` int(11) NOT NULL AUTO_INCREMENT,"
-    "  `prod_nom` varchar(150) NOT NULL,"
-    "  `prod_code` int NOT NULL,"
-    "  `prod_url` varchar(150) NOT NULL,"
-    "  `prod_store` varchar(150) NULL,"
-    "  `cat_id` int(11) NOT NULL,"
-    "  `nut_id` int(11) NOT NULL,"
-    "  `prodfab_id` int(11) NOT NULL,"
-    "  PRIMARY KEY (`prod_id`),"
-    "  CONSTRAINT `fk_cat_id` FOREIGN KEY (`cat_id`) "
-    "     REFERENCES `categories` (`cat_id`),"
-    "  CONSTRAINT `fk_nut_id` FOREIGN KEY (`nut_id`) "
-    "     REFERENCES `nutriscore` (`nut_id`)"
-    ") ENGINE=InnoDB")
-
-TABLES['sauvegardes'] = (
-    "CREATE TABLE IF NOT EXISTS`sauvegardes` ("
-    "  `save_id` int(11) NOT NULL AUTO_INCREMENT,"
-    "  `prod_id` int(11) NOT NULL,"
-    "  `save_time` datetime NOT NULL,"
-    "  PRIMARY KEY (`save_id`),"
-    "  CONSTRAINT `fk_saveprod_id` FOREIGN KEY (`prod_id`) "
-    "     REFERENCES `produits` (`prod_id`)"
-    ") ENGINE=InnoDB")
-
-TABLES['prodfab'] = (
-    "CREATE TABLE IF NOT EXISTS `prodfab` ("
-    "  `prodfab_id` int(11) NOT NULL AUTO_INCREMENT,"
-    "  `mar_id` int(11) NOT NULL,"
-    "  `prod_id` int(11) NOT NULL,"
-    "  PRIMARY KEY (`prodfab_id`),"
-    "  CONSTRAINT `fk_mar_id` FOREIGN KEY (`mar_id`) "
-    "     REFERENCES `marques` (`mar_id`),"
-    "  CONSTRAINT `fk_prodfabprod_id` FOREIGN KEY (`prod_id`) "
-    "     REFERENCES `produits` (`prod_id`)"
-    ") ENGINE=InnoDB")
 
 # Connection and cursor creation
 
@@ -91,7 +28,7 @@ def create_database(cursor):
 # Open the database
 
 try:
-    cursor.execute("USE {}".format(DB_NAME))
+    cursor.execute("USE {}".format(db_settings.DB_NAME))
 except mysql.connector.Error as err:
     print("Database {} does not exists.".format(DB_NAME))
     if err.errno == errorcode.ER_BAD_DB_ERROR:
@@ -104,8 +41,8 @@ except mysql.connector.Error as err:
 
 # Create tables
 
-for table_name in TABLES:
-    table_description = TABLES[table_name]
+for table_name in db_settings.TABLES:
+    table_description = db_settings.TABLES[table_name]
     try:
         print("Creating table {}: ".format(table_name), end='')
         cursor.execute(table_description)
