66bf3268bac4f09dd0267332e6811e722724f585
Eri Schon <erischon@gmail.com>
2020-11-14 15:34:55 +0100

    Update product_fetcher.py

--- environments/oc-p5/product_fetcher.py
+++ environments/oc-p5/product_fetcher.py
@@ -4,7 +4,10 @@ import json
 
 URL = "https://fr.openfoodfacts.org/cgi/search.pl"
 
+HEADERS = {"User-Agent": "P5_substitute - GNU/Windows - Version 0.1"}
+
 PARAMS = {
+    "search_simple": 1,
     "action": "process",
     "json": 1,
     "tagtype_0": "countries",
@@ -13,11 +16,11 @@ PARAMS = {
     "page_size": 20, 
     "page": 1,
     "sort_by": "unique_scans_n",
-
+    "fields": "generic_name_fr",
 }
 
 # GET request 
-response = requests.get(url = URL, params = PARAMS) 
+response = requests.get(url = URL, params = PARAMS, headers = HEADERS) 
 
 # extracting data in json format 
 products = response.json() 
