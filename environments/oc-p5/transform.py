023cba0ee8d2dae7109170b919b8190a214be98c
Eri Schon <erischon@gmail.com>
2020-11-16 09:49:50 +0100

    Update transform.py

--- environments/oc-p5/transform.py
+++ environments/oc-p5/transform.py
@@ -18,22 +18,19 @@ tampon = {}
 
 for n in range(len(data['products'])):
 
-    data_clean[data['products'][n]['product_name_fr'].lower()] = {}
+    data_clean[data['products'][n][fields[0]].lower()] = {}
 
-    data_clean[data['products'][n]['product_name_fr'].lower()]['code'] = data['products'][n]['code'].lower()
-    data_clean[data['products'][n]['product_name_fr'].lower()]['categories'] = data['products'][n]['categories'].lower()
-    data_clean[data['products'][n]['product_name_fr'].lower()]['nutriscrore_grade'] = data['products'][n]['nutriscore_grade'].lower()
-    data_clean[data['products'][n]['product_name_fr'].lower()]['url'] = data['products'][n]['url'].lower()
-    data_clean[data['products'][n]['product_name_fr'].lower()]['brands'] = data['products'][n]['brands'].lower()
-    data_clean[data['products'][n]['product_name_fr'].lower()]['stores'] = data['products'][n]['stores'].lower()
+    # data_clean[data['products'][n]['product_name_fr'].lower()]['code'] = data['products'][n]['code'].lower()
+    # data_clean[data['products'][n]['product_name_fr'].lower()]['categories'] = data['products'][n]['categories'].lower()
+    # data_clean[data['products'][n]['product_name_fr'].lower()]['nutriscrore_grade'] = data['products'][n]['nutriscore_grade'].lower()
+    # data_clean[data['products'][n]['product_name_fr'].lower()]['url'] = data['products'][n]['url'].lower()
+    # data_clean[data['products'][n]['product_name_fr'].lower()]['brands'] = data['products'][n]['brands'].lower()
+    # data_clean[data['products'][n]['product_name_fr'].lower()]['stores'] = data['products'][n]['stores'].lower()
 
-    # for field in fields:
-    #     # print(field, data['products'][n][field].lower())
-
-    #     if field == 'product_name_fr':
-    #         tampon[data['products'][n][field].lower()] = {}
-    #     else:
-    #         pass
-
-
-print(data_clean)
+    for field in fields:
+        if field != fields[0]:
+            data_clean[data['products'][n][fields[0]].lower()][field] = data['products'][n][field].lower()
+        
+ 
+with open('products_fr', 'w') as fp:
+    json.dump(data_clean, fp)
