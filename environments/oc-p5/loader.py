954d2250e07709f502e588f141e4efe27b056349
Eri Schon <erischon@gmail.com>
2020-11-17 11:12:46 +0100

    Update loader.py

--- environments/oc-p5/loader.py
+++ environments/oc-p5/loader.py
@@ -6,28 +6,63 @@ from mysql.connector import errorcode
 from db_settings import Database
 
 
-with open('my_products_fr.json', encoding='utf-8') as json_file:
-    my_products = json.load(json_file)
+class Loader:
 
-db_settings = Database()
-database_name = db_settings.DB_NAME
+    def __init__(self):
+        pass
 
-cnx = mysql.connector.connect(
-    host = HOST,
-    user = USER,
-    password = PASSWORD,
-    database = db_settings.DB_NAME)
+    def db_connect(self):
 
-mycursor = cnx.cursor()
+        try:
+            db_settings = Database()
+            database_name = db_settings.DB_NAME
 
-try:
-    add_nutriscore = ("INSERT INTO nutriscore (nut_id, nut_type) VALUES (NULL,%s)")
-    val = ('A')
+            connection = mysql.connector.connect(
+                host = HOST,
+                user = USER,
+                password = PASSWORD,
+                database = db_settings.DB_NAME)
 
-    mycursor.execute(add_nutriscore, (val, ))
-    cnx.commit()
+            mycursor = connection.cursor()
 
-except mysql.connector.Error as error:
-    print("Failed to insert into MySQL table {}".format(error))
+            if (connection.is_connected()):
+                print("Connection : OK")
 
-print(mycursor.rowcount, "record inserted.")
+        except mysql.connector.Error as error:
+            print("Failed to insert into MySQL table {}".format(error))
+
+        # finally:
+        #     if (connection.is_connected()):
+        #         mycursor.close()
+        #         connection.close()
+        #         print("MySQL connection is closed")
+        
+
+    def open_json(self):
+        with open('my_products_fr.json', encoding='utf-8') as json_file:
+            my_products = json.load(json_file)
+
+        print(my_products)
+
+    def load_data(self):
+        pass
+
+
+if __name__ == "__main__":
+    loader = Loader()
+
+# loader.open_json()
+loader.db_connect()
+
+
+# try:
+#     add_nutriscore = ("INSERT INTO nutriscore (nut_id, nut_type) VALUES (NULL,%s)")
+#     val = ('A')
+
+#     mycursor.execute(add_nutriscore, (val, ))
+#     cnx.commit()
+
+# except mysql.connector.Error as error:
+#     print("Failed to insert into MySQL table {}".format(error))
+
+# print(mycursor.rowcount, "record inserted.")
