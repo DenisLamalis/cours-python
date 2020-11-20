b44bca47d2f82669e51a95040151c2f95d3cd821
Eri Schon <erischon@gmail.com>
2020-11-20 10:16:39 +0100

    Update database.py

--- environments/oc-p5/database.py
+++ environments/oc-p5/database.py
@@ -1,15 +1,17 @@
-from config import *
 import mysql.connector
+
+from config import *
 from mysql.connector import errorcode
 
 class Database:
     """ """
 
     def __init__(self):
+        """ """
         self.host = HOST
         self.user = USER
         self.password = PASSWORD
-        self.database = 'purebeurre'
+        self.db_name = 'PureBeurre'
 
     def connection(self):
         """ """
@@ -19,12 +21,12 @@ class Database:
             host = self.host,
             user = self.user,
             password = self.password,
-            database = self.database)
+            database = self.db_name)
 
             self.mycursor = self.connection.cursor()
 
             if (self.connection.is_connected()):
-                print(f"Connection à la base {self.database} : OK")
+                print(f"Connection à la base {self.db_name} : OK")
 
         except mysql.connector.Error as error:
             print("Echec : impossible de me connecté, {}".format(error))
@@ -33,4 +35,4 @@ class Database:
 if __name__ == "__main__":
     database = Database()
 
-# database.connection()
+database.connection()
