5058b48f10576a377656c0729b51f64b3dd9e31f
Eri Schon <erischon@gmail.com>
2020-11-20 10:53:45 +0100

    Update database.py

--- environments/oc-p5/database.py
+++ environments/oc-p5/database.py
@@ -2,6 +2,7 @@ import mysql.connector
 
 from config import *
 from mysql.connector import errorcode
+from tables import Tables
 
 class Database:
     """ """
@@ -12,6 +13,7 @@ class Database:
         self.user = USER
         self.password = PASSWORD
         self.db_name = 'PureBeurre'
+        self.tables = Tables()
 
     def connection(self):
         """ """
@@ -26,13 +28,40 @@ class Database:
             self.mycursor = self.connection.cursor()
 
             if (self.connection.is_connected()):
-                print(f"Connection à la base {self.db_name} : OK")
+                print(f"REUSSITE : Connection à la base {self.db_name} effectuée.")
+            
+            return self.mycursor
 
         except mysql.connector.Error as error:
-            print("Echec : impossible de me connecté, {}".format(error))
+            print("ECHEC : impossible de me connecter, erreur : {}".format(error))
 
+    def db_create(self):
+        """ """
+        mycursor = self.connection()
+        
+        try:
+            mycursor.execute("CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(self.db_name))
+            print(f"REUSSITE : création de la base {self.db_name} effectuée.")
+        
+        except mysql.connector.Error as err:
+            print("ECHEC : impossible de créer la base, erreur : {}".format(err))
+            exit(1)
+
+    def tables_create(self):
+        """ """
+        mycursor = self.connection()
+
+        for table_name in self.tables.TABLES:
+            table_description = self.tables.TABLES[table_name]
+            
+            try:
+                mycursor.execute(table_description)
+                print("REUSSITE : la création de la table {} est effectuée.\n".format(table_name), end='')
+            
+            except mysql.connector.Error as err:
+                print("ECHEC : impossible de créer la table, erreur : {}".format(error))
 
 if __name__ == "__main__":
     database = Database()
 
-database.connection()
+database.tables_create()
