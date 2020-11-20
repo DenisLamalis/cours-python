3a49d6c9b58e545d495ecb7920b9667d32dd53ac
Eri Schon <erischon@gmail.com>
2020-11-20 10:53:58 +0100

    Create script.py

---
+++ environments/oc-p5/my_tests/script.py
@@ -0,0 +1,16 @@
+from data import *
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host = HOST,
+  user = USER,
+  password = PASSWORD
+)
+
+mycursor = mydb.cursor()
+
+# mycursor.execute("CREATE DATABASE PureBeurre CHARACTER SET 'utf8'")
+
+mycursor.execute("USE PureBeurre;")
+mycursor.execute("SHOW TABLES;")
+
