962c85997e83e64a62b88d2bfa147dc45f288572
Eri Schon <erischon@gmail.com>
2020-11-21 17:34:02 +0100

    Update tab_modeles.py

--- environments/oc-p5/tab_modeles.py
+++ environments/oc-p5/tab_modeles.py
@@ -1,7 +1,31 @@
 class ModCategories:
     """ """
-
     def __init__(self):
         self.id_target = 'cat_id'
         self.table_target = 'categories'
         self.column_target = 'cat_nom'
+
+class ModMarque:
+    """ """
+    def __init__(self):
+        id_target = 'marq_id'
+        table_target = 'marques'
+        column_target = 'marq_nom'
+        product_target = value
+
+class ModShop:
+    """ """
+    def __init__(self):
+        id_target = 'shop_id'
+        table_target = 'shops'
+        column_target = 'shop_nom'
+        product_target = value
+
+class ModProduits:
+    """ """
+    def __init__(self):
+        id_target = 'prod_id'
+        table_target = 'produits'
+        column_target = 'prod_id'
+        product_target = value
+
